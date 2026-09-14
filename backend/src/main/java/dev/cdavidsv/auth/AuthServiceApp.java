64686db4402a68525576aa527e0821b7776b9030
Carlos Sandoval <cdavidsv6@gmail.com>
2026-09-14 14:58:59 -0700

    Added initial project structure using Spring Boot

---
+++ backend/src/main/java/dev/cdavidsv/auth/AuthServiceApp.java
@@ -0,0 +1,11 @@
+package dev.cdavidsv.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApp {
+    static void main(String[] args) {
+        SpringApplication.run(AuthServiceApp.class, args);
+    }
+}
